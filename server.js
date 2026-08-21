877bd1509221f76f703d547a778261867693366f
Js0nZ <mazaa1452@gmail.com>
2026-08-21 10:30:11 +0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import crypto from 'crypto';
